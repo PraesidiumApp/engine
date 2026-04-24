5b8f68a36c4dd9ff2c0bc24b0ee5f5415363e24b
Germán <germanrodriguezvillar@bonai.es>
2026-04-24 17:52:52 +0200

    feat: Session open() constructor

--- src/error.rs
+++ src/error.rs
@@ -16,12 +16,8 @@ pub enum Error {
     Cipher(#[from] aes_gcm::Error),
     #[error("Database error")]
     DB(#[from] rusqlite::Error),
-    #[error("Vault operation error")]
-    Vault(VaultError),
-}
-
-#[derive(Error, Debug)]
-pub enum VaultError {
-    #[error("Attempted to create a new vault on a not empty database")]
+    #[error("Can't create a new vault on a not empty database")]
     VaultNotEmpty,
+    #[error("Vault version is newer than what we support")]
+    VaultVersionNewer
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,24 @@ pub mod error;
 
 use crate::{
     crypto::{MASTER_KEY_SIZE, SALT_SIZE, derive_master_key, fill_with_random_bytes},
-    error::{Error, VaultError},
+    error::Error,
 };
-use rusqlite::Connection;
+use rusqlite::{Connection, OpenFlags};
 use std::path::Path;
-use zeroize::{Zeroize, Zeroizing};
+use zeroize::Zeroizing;
 
-pub const VAULT_VERSION: isize = 1;
+pub const VAULT_VERSION: u32 = 1;
 
 pub struct Session {
-    vault: Connection,
+    connection: Connection,
     master_key: Zeroizing<[u8; MASTER_KEY_SIZE]>,
+    metadata: SessionMetadata,
+}
+
+struct SessionMetadata {
+    salt: [u8; SALT_SIZE],
+    created_at: String,
+    version: u32
 }
 
 impl Session {
@@ -24,14 +31,14 @@ impl Session {
     /// * `path` - Any type that can be converted to a [Path] by the [AsRef] trait
     /// * `password` - A text master password used to protect items inside the new vault
     /// # Errors
-    /// * `Err(Error::DB(...))` - If there was any problem trying to create, connect or query to the database
+    /// * `Err(Error::DB(...))` - If there was any problem trying to connect or query to the database
     /// * `Err(Error::RNG(...))` - If there was any problem with the random number generator
 	/// * `Err(Error::KDF(...))` - If there was any problem with the key derivation function
-    /// * `Err(Error::Vault(...))` - If the provided path IS a SQLite database and is NOT empty
+    /// * `Err(Error::VaultNotEmpty)` - If the provided path IS a SQLite database and is NOT empty
     pub fn new<P: AsRef<Path>>(path: P, password: &mut str) -> Result<Self, Error> {
         let vault_connection = Connection::open(path)?;
 
-        // If file exists (it is not supposed to) and its not a SQLite database it should error
+        // If file exists (it is not supposed to) and its not a SQLite database it should error (rusqlite)
         // If it IS a SQLite database check its empty before initializing it
         // It would be better to have an atomic operation in rusqlite to guarantee the opened
         // file is new and avoid TOCTOU race conditions but AFAIK there is no such option
@@ -43,7 +50,7 @@ impl Session {
 
         if table_count != 0 {
             // If it's not 0, it's not a new database
-            return Err(Error::Vault(VaultError::VaultNotEmpty));
+            return Err(Error::VaultNotEmpty);
         }
 
         let mut salt_buffer = [0u8; SALT_SIZE];
@@ -52,12 +59,10 @@ impl Session {
         let mut master_key_buffer = [0u8; MASTER_KEY_SIZE];
         derive_master_key(password, &salt_buffer, &mut master_key_buffer)?;
 
-        password.zeroize();
-
-        // Prepare database layout
+        // Prepare database schema
         vault_connection.execute_batch(
             "
-			-- Table for global vault settings (Only 1 row ever)
+			-- Vault metadata (Only 1 row ever)
 			CREATE TABLE metadata (
 				id INTEGER PRIMARY KEY CHECK (id = 1), 
 				salt BLOB NOT NULL,
@@ -65,26 +70,77 @@ impl Session {
 				version INTEGER NOT NULL
 			);
 
-			-- Table for vault items
+			-- Vault items
 			CREATE TABLE items (
 				id INTEGER PRIMARY KEY,
 				label TEXT NOT NULL,
+                type TEXT NOT NULL,
 				nonce BLOB NOT NULL,
 				auth_tag BLOB NOT NULL,
 				ciphertext BLOB NOT NULL
 			);
-		",
+		    ",
         )?;
 
-        // Insert initial metadata
+        // Insert initial metadata, created_at is inserted by SQLite
         vault_connection.execute(
             "INSERT INTO metadata (id, salt, version) VALUES (1, ?1, ?2)",
             (salt_buffer.as_slice(), VAULT_VERSION),
         )?;
 
+        let vault_metadata = SessionMetadata::get(&vault_connection)?;
+
         Ok(Self {
-            vault: vault_connection,
+            connection: vault_connection,
             master_key: Zeroizing::new(master_key_buffer),
+            metadata: vault_metadata
         })
+    }
+
+    /// Create a new [Session], opening an existing underlying vault
+    /// # Arguments
+    /// * `path` - Any type that can be converted to a [Path] by the [AsRef] trait
+    /// * `password` - A text master password used to unlock items inside the vault
+    /// # Errors
+    /// * `Err(Error::DB(...))` - If there was any problem trying to connect or query to the database
+	/// * `Err(Error::KDF(...))` - If there was any problem with the key derivation function
+    pub fn open<P: AsRef<Path>>(path: P, password: &mut str) -> Result<Self, Error> {
+        let mut vault_connection_flags = OpenFlags::default();
+        vault_connection_flags.remove(OpenFlags::SQLITE_OPEN_CREATE);
+
+        let vault_connection = Connection::open_with_flags(path, vault_connection_flags)?;
+
+        let vault_metadata = SessionMetadata::get(&vault_connection)?;
+
+        if vault_metadata.version > VAULT_VERSION {
+            return Err(Error::VaultVersionNewer)
+        }
+
+        let mut master_key_buffer = [0u8; MASTER_KEY_SIZE];
+        derive_master_key(password, &vault_metadata.salt, &mut master_key_buffer)?;
+
+        Ok(
+            Self {
+                connection: vault_connection,
+                master_key: Zeroizing::new(master_key_buffer),
+                metadata: vault_metadata
+            }
+        )
+    }
+}
+
+impl SessionMetadata {
+    fn get(connection: &Connection) -> Result<Self, Error> {
+        Ok(
+            connection.query_row(
+                "SELECT salt, created_at, version FROM metadata WHERE id = 1",
+                [],
+                |row| Ok(SessionMetadata {
+                    salt: row.get(0)?,
+                    created_at: row.get(1)?,
+                    version: row.get(2)?,
+                })
+            )?
+        )
     }
 }
